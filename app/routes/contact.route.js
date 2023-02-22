f20f968d92a505abf13ccf0996b52f5c7561e7c6
ThanhPhu2 <phub1910124@student.ctu.edu.vn>
2023-02-22 17:47:08 +0700

    Cai dat du an

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const { route } = require("../../app");
-const contacts = require("../controllers/contact.controller.js");
+const contacts = require("../controllers/contact.controller");
 
 const router = express.Router();
 
